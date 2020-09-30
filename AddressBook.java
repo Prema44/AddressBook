9cd869258ca92913a9d4d182434ab278ed6aba6e
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-30 08:25:14 +0530

    Multiple AddresssBook

--- AddressBook.java
+++ AddressBook.java
@@ -5,26 +5,33 @@ import java.util.Scanner;
 
 public class AddressBook {
 	
-    public ArrayList<Contact> addressBook = new ArrayList<Contact>();
-	
+	public String name;
+	public AddressBook(String name) {
+		this.name=name;
+	}
+	public Contact contact;
+	public ArrayList<Contact> addressBook = new ArrayList<Contact>();
+	public Contact getContact() {
+		return contact;
+	}
+	public void setContact(Contact contact) {
+		this.contact = contact;
+	}
 	public ArrayList<Contact> getAddressBook() {
 		return addressBook;
 	}
-	
 	public void setAddressBook(ArrayList<Contact> addressbook) {
 		this.addressBook = addressBook;
 	}
-	
 	public void addContact(Contact contact) {
 		addressBook.add(contact);
 	}
-	
 	public void editContact(String name) {
 		Scanner sc = new Scanner(System.in);
-		String x = "";
+		String editName = "";
 		for(Contact contact : addressBook) {
-			x = contact.getFirstName() + contact.getLastName();
-			if(name.equals(x)) {
+			editName = contact.getFirstName() + contact.getLastName();
+			if(name.equals(editName)) {
 				System.out.println("1.Change the address");
 				System.out.println("2.Change the city");
 				System.out.println("3.Change the state");
@@ -62,18 +69,16 @@ public class AddressBook {
 				break;
 				}	
 			}
-			else 
-			{
-				 System.out.println("No such contact exist");
+			else {
+				System.out.println("No such contact exist");
 			}
 		}
 	}
-	
 	public void deleteContact(String name) {
-		String x = "";
+		String deleteName = "";
 		for(Contact contact : addressBook) {
-			x = contact.getFirstName() + contact.getLastName();
-			if(name.equals(x)) {
+			deleteName = contact.getFirstName() + contact.getLastName();
+			if(name.equals(deleteName)) {
 				addressBook.remove(contact);
 			}
 		}
